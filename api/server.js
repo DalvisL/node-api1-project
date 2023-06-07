dfc5a173c046dd9a12b46a289b8653e3dcc9ec2f
Dalvis <Dalvis.liz27@gmail.com>
2023-06-07 17:08:48 -0400

    Got server to run on port 9000

--- api/server.js
+++ api/server.js
@@ -1,3 +1,12 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.status(200).json({ message: 'hello world' });
+    
+});
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
